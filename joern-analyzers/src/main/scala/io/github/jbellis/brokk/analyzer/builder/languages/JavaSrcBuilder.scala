ab0cd4d1cbb5c23e5412445a8e14d82a739eda06
Carlos Quiroz <212936523+cmanto@users.noreply.github.com>
2025-08-14 18:30:46 -0400

    fix: JavaSrcBuilder to use direct run() calls for binary compatibility

--- joern-analyzers/src/main/scala/io/github/jbellis/brokk/analyzer/builder/languages/JavaSrcBuilder.scala
+++ joern-analyzers/src/main/scala/io/github/jbellis/brokk/analyzer/builder/languages/JavaSrcBuilder.scala
@@ -20,17 +20,80 @@ object JavaSrcBuilder {
 
     override def createAst(cpg: Cpg, config: JavaSrcConfig)(using pool: ForkJoinPool): Try[Cpg] = Try {
       createOrUpdateMetaData(cpg, Languages.JAVASRC, config.inputPath)
+
+      // BINARY COMPATIBILITY FIX: Use direct run() calls instead of createAndApply()
+      // because Joern's passes expect createAndApply(ForkJoinPool) but our local
+      // CpgPassBase interface defines createAndApply() with implicit ForkJoinPool parameter
+
       val astCreationPass = new AstCreationPass(config, cpg)
-      astCreationPass.createAndApply()
+      val diffBuilder = io.shiftleft.codepropertygraph.generated.Cpg.newDiffGraphBuilder
+
+      // Manual execution of pass parts since runWithBuilder has binary compatibility issues
+      astCreationPass.init()
+      val parts = astCreationPass.generateParts()
+      for (part <- parts) {
+        astCreationPass.runOnPart(diffBuilder, part)
+      }
+      astCreationPass.finish()
+
+      flatgraph.DiffGraphApplier.applyDiff(cpg.graph, diffBuilder)
+
       astCreationPass.sourceParser.cleanupDelombokOutput()
       astCreationPass.clearJavaParserCaches()
-      new OuterClassRefPass(cpg).createAndApply()
-      JavaConfigFileCreationPass(cpg).createAndApply()
+
+      val outerClassRefPass = new OuterClassRefPass(cpg)
+      val outerClassDiffBuilder = io.shiftleft.codepropertygraph.generated.Cpg.newDiffGraphBuilder
+
+      // Manual execution for binary compatibility
+      outerClassRefPass.init()
+      val outerClassParts = outerClassRefPass.generateParts()
+      for (part <- outerClassParts) {
+        outerClassRefPass.runOnPart(outerClassDiffBuilder, part)
+      }
+      outerClassRefPass.finish()
+
+      flatgraph.DiffGraphApplier.applyDiff(cpg.graph, outerClassDiffBuilder)
+
+      val javaConfigPass = JavaConfigFileCreationPass(cpg)
+      val javaConfigDiffBuilder = io.shiftleft.codepropertygraph.generated.Cpg.newDiffGraphBuilder
+
+      // Manual execution for binary compatibility
+      javaConfigPass.init()
+      val javaConfigParts = javaConfigPass.generateParts()
+      for (part <- javaConfigParts) {
+        javaConfigPass.runOnPart(javaConfigDiffBuilder, part)
+      }
+      javaConfigPass.finish()
+
+      flatgraph.DiffGraphApplier.applyDiff(cpg.graph, javaConfigDiffBuilder)
+
       if (!config.skipTypeInfPass) {
-        TypeNodePass
+        val typeNodePass = TypeNodePass
           .withRegisteredTypes(astCreationPass.global.usedTypes.keys().asScala.toList, cpg)
-          .createAndApply()
-        new TypeInferencePass(cpg).createAndApply()
+        val typeNodeDiffBuilder = io.shiftleft.codepropertygraph.generated.Cpg.newDiffGraphBuilder
+
+        // Manual execution for binary compatibility
+        typeNodePass.init()
+        val typeNodeParts = typeNodePass.generateParts()
+        for (part <- typeNodeParts) {
+          typeNodePass.runOnPart(typeNodeDiffBuilder, part)
+        }
+        typeNodePass.finish()
+
+        flatgraph.DiffGraphApplier.applyDiff(cpg.graph, typeNodeDiffBuilder)
+
+        val typeInferencePass = new TypeInferencePass(cpg)
+        val typeInferenceDiffBuilder = io.shiftleft.codepropertygraph.generated.Cpg.newDiffGraphBuilder
+
+        // Manual execution for binary compatibility
+        typeInferencePass.init()
+        val typeInferenceParts = typeInferencePass.generateParts()
+        for (part <- typeInferenceParts) {
+          typeInferencePass.runOnPart(typeInferenceDiffBuilder, part)
+        }
+        typeInferencePass.finish()
+
+        flatgraph.DiffGraphApplier.applyDiff(cpg.graph, typeInferenceDiffBuilder)
       }
       cpg
     }
