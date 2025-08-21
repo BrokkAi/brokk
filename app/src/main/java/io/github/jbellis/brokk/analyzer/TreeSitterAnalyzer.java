03768a94170efc1521f0b681fd9935e4769cc9d2
David Baker Effendi <david@brokk.ai>
2025-08-21 10:36:54 +0200

    Merge tweak

--- app/src/main/java/io/github/jbellis/brokk/analyzer/TreeSitterAnalyzer.java
+++ app/src/main/java/io/github/jbellis/brokk/analyzer/TreeSitterAnalyzer.java
@@ -27,6 +27,7 @@ import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Function;
 import java.util.function.Predicate;
