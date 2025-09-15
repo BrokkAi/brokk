784db10dec17458a4c9bef8789dbc8da45edef74
Ryan SVIHLA <86598053+foundev@users.noreply.github.com>
2025-09-16 00:16:59 +0200

    UI: Use icons for add/remove buttons in Settings panels.

--- app/src/main/java/io/github/jbellis/brokk/gui/dialogs/SettingsProjectPanel.java
+++ app/src/main/java/io/github/jbellis/brokk/gui/dialogs/SettingsProjectPanel.java
@@ -12,6 +12,7 @@ import io.github.jbellis.brokk.analyzer.Languages;
 import io.github.jbellis.brokk.gui.Chrome;
 import io.github.jbellis.brokk.gui.GuiTheme;
 import io.github.jbellis.brokk.gui.ThemeAware;
+import io.github.jbellis.brokk.gui.util.Icons;
 import io.github.jbellis.brokk.gui.dialogs.analyzer.AnalyzerSettingsPanel;
 import io.github.jbellis.brokk.issues.FilterOptions;
 import io.github.jbellis.brokk.issues.IssuesProviderConfig;
@@ -77,8 +78,8 @@ public class SettingsProjectPanel extends JPanel implements ThemeAware {
     private DefaultListModel<String> excludedDirectoriesListModel = new DefaultListModel<>();
     private JList<String> excludedDirectoriesList = new JList<>(excludedDirectoriesListModel);
     private JScrollPane excludedScrollPane = new JScrollPane(excludedDirectoriesList);
-    private JButton addExcludedDirButton = new JButton("Add");
-    private JButton removeExcludedDirButton = new JButton("Remove");
+    private JButton addExcludedDirButton = new JButton(Icons.ADD);
+    private JButton removeExcludedDirButton = new JButton(Icons.REMOVE);
 
     private JTextField languagesDisplayField = new JTextField(20);
     private JButton editLanguagesButton = new JButton("Edit");
