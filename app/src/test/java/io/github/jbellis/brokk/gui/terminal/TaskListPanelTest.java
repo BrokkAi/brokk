05302126b806360322344323e914ed6ec20416a8
Ryan SVIHLA <86598053+foundev@users.noreply.github.com>
2025-09-30 18:26:36 +0200

    test: Add constants for editable fragments in TaskListPanelTest

--- app/src/test/java/io/github/jbellis/brokk/gui/terminal/TaskListPanelTest.java
+++ app/src/test/java/io/github/jbellis/brokk/gui/terminal/TaskListPanelTest.java
@@ -7,19 +7,22 @@ import org.junit.jupiter.api.Test;
 
 public class TaskListPanelTest {
 
+    private static final boolean HAS_EDITABLE_FRAGMENTS = true;
+    private static final boolean NO_EDITABLE_FRAGMENTS = false;
+
     @Test
     void skipSearch_whenFirstTaskAndHasEditableFragments() {
-        assertTrue(TaskListPanel.shouldSkipSearchForTask(0, true));
+        assertTrue(TaskListPanel.shouldSkipSearchForTask(0, HAS_EDITABLE_FRAGMENTS));
     }
 
     @Test
     void doNotSkipSearch_whenFirstTaskAndNoEditableFragments() {
-        assertFalse(TaskListPanel.shouldSkipSearchForTask(0, false));
+        assertFalse(TaskListPanel.shouldSkipSearchForTask(0, NO_EDITABLE_FRAGMENTS));
     }
 
     @Test
     void doNotSkipSearch_whenNotFirstTask_evenIfHasEditableFragments() {
-        assertFalse(TaskListPanel.shouldSkipSearchForTask(1, true));
-        assertFalse(TaskListPanel.shouldSkipSearchForTask(2, false));
+        assertFalse(TaskListPanel.shouldSkipSearchForTask(1, HAS_EDITABLE_FRAGMENTS));
+        assertFalse(TaskListPanel.shouldSkipSearchForTask(2, NO_EDITABLE_FRAGMENTS));
     }
 }
