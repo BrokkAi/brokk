86ccd8ab9ce5459576b6752e3514bc35c2b382f2
David Baker Effendi <david@brokk.ai>
2025-10-24 13:42:01 +0200

    Remove last fix attempt

--- app/src/test/java/io/github/jbellis/brokk/context/HistoryV4MigrationTest.java
+++ app/src/test/java/io/github/jbellis/brokk/context/HistoryV4MigrationTest.java
@@ -3,7 +3,6 @@ package io.github.jbellis.brokk.context;
 import static org.junit.jupiter.api.Assertions.*;
 
 import io.github.jbellis.brokk.IContextManager;
-import io.github.jbellis.brokk.MainProject;
 import io.github.jbellis.brokk.testutil.NoOpConsoleIO;
 import io.github.jbellis.brokk.testutil.TestContextManager;
 import io.github.jbellis.brokk.util.HistoryIo;
@@ -40,7 +39,6 @@ class HistoryV4MigrationTest {
 
     @BeforeEach
     void setup() throws IOException {
-        MainProject.setLlmProxySetting(MainProject.LlmProxySetting.LOCALHOST);
         Path projectRoot = tempDir.resolve("project");
         Files.createDirectories(projectRoot);
         mockContextManager = new TestContextManager(projectRoot, new NoOpConsoleIO());
