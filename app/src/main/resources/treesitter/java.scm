17c51a1e8b498f37e3a9873e92ab384c3b8ec0c2
David Baker Effendi <david@brokk.ai>
2025-11-12 10:26:35 +0200

    Fixed interface extends query

--- app/src/main/resources/treesitter/java.scm
+++ app/src/main/resources/treesitter/java.scm
@@ -80,14 +80,14 @@
 
 (interface_declaration
   name: (identifier) @type.name
-  interfaces: (super_interfaces
-                (type_list
-                  [
-                    (type_identifier)
-                    (scoped_type_identifier)
-                  ] @type.super
-                )
-              )?
+  (extends_interfaces
+    (type_list
+      [
+        (type_identifier)
+        (scoped_type_identifier)
+        ] @type.super
+      )
+    )?
 ) @type.decl
 
 (enum_declaration
