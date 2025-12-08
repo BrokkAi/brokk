af7c750d41c5737ee11cb8c6fead512d76e25589
Jonathan Ellis <jbellis@gmail.com>
2025-12-08 12:20:56 -0600

    enh: InstructionsPanel use formatted completion text

--- app/src/main/java/ai/brokk/gui/InstructionsPanel.java
+++ app/src/main/java/ai/brokk/gui/InstructionsPanel.java
@@ -2950,7 +2950,7 @@ public class InstructionsPanel extends JPanel implements IContextManager.Context
                         ProjectFile::getFileName,
                         ProjectFile::toString,
                         f -> 0,
-                        f -> new ShorthandCompletion(this, f.getFileName(), f.toString()));
+                        f -> new ShorthandCompletion(this, f.toString(), formatCompletionText(f.toString())));
                 completions = new ArrayList<>(fileCompletions.stream().limit(50).toList());
             } else {
                 var analyzer = contextManager.getAnalyzerWrapper().getNonBlocking();
