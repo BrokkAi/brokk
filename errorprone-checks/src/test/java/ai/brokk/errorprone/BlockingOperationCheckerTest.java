54e8eb21f07544974d2621f74489fbddcd024fc1
David Baker Effendi <david@brokk.ai>
2026-01-19 11:06:08 +0200

    test: add case for nested blocking call in submitBackgroundTask to BlockingOperationCheckerTest

--- errorprone-checks/src/test/java/ai/brokk/errorprone/BlockingOperationCheckerTest.java
+++ errorprone-checks/src/test/java/ai/brokk/errorprone/BlockingOperationCheckerTest.java
@@ -258,4 +258,46 @@ public class BlockingOperationCheckerTest {
                         "}")
                 .doTest();
     }
+
+    @Test
+    public void doesNotWarnOnNestedBlockingCallInSubmitBackgroundTask() {
+        helper.addSourceLines(
+                        "org/jetbrains/annotations/Blocking.java",
+                        "package org.jetbrains.annotations;",
+                        "public @interface Blocking {}")
+                .addSourceLines(
+                        "test/IContextManager.java",
+                        "package test;",
+                        "import java.util.concurrent.CompletableFuture;",
+                        "import java.util.concurrent.Callable;",
+                        "public interface IContextManager {",
+                        "  <T> CompletableFuture<T> submitBackgroundTask(String desc, Callable<T> task);",
+                        "}")
+                .addSourceLines(
+                        "test/CF.java",
+                        "package test;",
+                        "import org.jetbrains.annotations.Blocking;",
+                        "public interface CF {",
+                        "  @Blocking",
+                        "  java.util.Set<String> files();",
+                        "}")
+                .addSourceLines(
+                        "test/Use.java",
+                        "package test;",
+                        "import javax.swing.SwingUtilities;",
+                        "import java.util.stream.Stream;",
+                        "class Use {",
+                        "  void f(CF cf, IContextManager cm) {",
+                        "    if (SwingUtilities.isEventDispatchThread()) {",
+                        "      cm.submitBackgroundTask(\"task\", () -> {",
+                        "        // Safe: nested deeply within the background task argument",
+                        "        return Stream.of(\"a\")",
+                        "            .map(s -> cf.files())",
+                        "            .count();",
+                        "      });",
+                        "    }",
+                        "  }",
+                        "}")
+                .doTest();
+    }
 }
