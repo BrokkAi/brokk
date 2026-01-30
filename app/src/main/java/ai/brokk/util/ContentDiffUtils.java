cfc07fe729f22528dd6cd5130ee1ed6d7b4a40b4
Jonathan Ellis <jbellis@gmail.com>
2026-01-30 13:05:12 -0600

    enh: ignore whitespace changes in ContentDiffUtils review diffs

--- app/src/main/java/ai/brokk/util/ContentDiffUtils.java
+++ app/src/main/java/ai/brokk/util/ContentDiffUtils.java
@@ -95,7 +95,7 @@ public class ContentDiffUtils {
         var oldLines = toLines(oldContent);
         var newLines = toLines(newContent);
 
-        Patch<String> patch = DiffUtils.diff(oldLines, newLines);
+        Patch<String> patch = DiffUtils.diff(oldLines, newLines, ContentDiffUtils::reviewLinesEqualIgnoringWhitespace);
         if (patch.getDeltas().isEmpty()) {
             return new DiffComputationResult("", 0, 0);
         }
@@ -539,6 +539,17 @@ public class ContentDiffUtils {
         }
 
         return String.join("\n", result);
+    }
+
+    private static boolean reviewLinesEqualIgnoringWhitespace(String a, String b) {
+        return stripAllWhitespace(a).equals(stripAllWhitespace(b));
+    }
+
+    private static String stripAllWhitespace(String s) {
+        return s.chars()
+                .filter(ch -> !Character.isWhitespace(ch))
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 
     /**
