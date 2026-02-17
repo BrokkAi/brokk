f05d7e2371ddce9005da226a5b9d7e5f9d0d3d42
Ryan SVIHLA <86598053+foundev@users.noreply.github.com>
2026-02-17 22:47:31 +0100

    test: clean up comment formatting in test_token_bar

--- brokk-code/tests/test_token_bar.py
+++ brokk-code/tests/test_token_bar.py
@@ -98,8 +98,8 @@ def test_render_bar_with_track():
     from textual.geometry import Size
 
     bar = TokenBar()
-    # " 500 / 1,000 tokens" is 18 chars. Need width for bar + text.
     bar._test_size = Size(40, 1)
+    # " 500 / 1,000 tokens" is 18 chars. Need width for bar + text.
     # used=500, max=1000. usage_str=" 500 / 1,000 tokens" (18 chars). bar_width=22.
     # fill=11. track=11.
     bar.update_tokens(500, 1000)
@@ -123,8 +123,8 @@ def test_render_bar_auto_rescale():
     from textual.geometry import Size
 
     bar = TokenBar()
-    # " 2,000 / 1,000 tokens" is 20 chars.
     bar._test_size = Size(40, 1)
+    # " 2,000 / 1,000 tokens" is 20 chars.
     # used=2000, max=1000. bar_width=20.
     # used > max, so effective_max=2000. fill should be 100% of bar_width.
     bar.update_tokens(2000, 1000)
