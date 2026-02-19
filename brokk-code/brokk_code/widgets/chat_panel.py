41828cb8d2a83639763934d64bbeabd2462055fc
Ryan SVIHLA <86598053+foundev@users.noreply.github.com>
2026-02-19 20:47:42 +0100

    UI: remove /commands from help text in chat_panel

--- brokk-code/brokk_code/widgets/chat_panel.py
+++ brokk-code/brokk_code/widgets/chat_panel.py
@@ -690,7 +690,7 @@ class ChatPanel(Vertical):
             yield LoadingIndicator(id="help-spinner", classes="hidden")
             yield Static(id="help-elapsed", classes="hidden")
             yield Static(
-                "Enter: Submit  Shift+Enter: Newline  Up/Down: History  Shift+Tab: Mode  /commands",
+                "Enter: Submit  Shift+Enter: Newline  Up/Down: History  Shift+Tab: Mode",
                 id="chat-help",
             )
 
