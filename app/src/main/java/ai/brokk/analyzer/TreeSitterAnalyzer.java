329a455da35e5a906d4902a695a12d018194e944
David Baker Effendi <david@brokk.ai>
2026-03-20 12:38:16 +0200

    Revert Directly Committed Go Optimisation Commits (#3158)

--- app/src/main/java/ai/brokk/analyzer/TreeSitterAnalyzer.java
+++ app/src/main/java/ai/brokk/analyzer/TreeSitterAnalyzer.java
@@ -451,15 +451,11 @@ public abstract class TreeSitterAnalyzer implements IAnalyzer, TypeAliasProvider
     }
 
     protected TreeSitterAnalyzer(IProject project, Language language, ProgressListener listener) {
-        this(project, language, listener, new AnalyzerCache());
-    }
-
-    protected TreeSitterAnalyzer(IProject project, Language language, ProgressListener listener, AnalyzerCache cache) {
         this.project = project;
         this.language = language;
         // Register listener early so it receives progress during construction
         progressListener = listener;
-        this.cache = cache;
+        this.cache = new AnalyzerCache();
 
         // Initialize query sources from resources.
         Map<QueryType, String> sources = new EnumMap<>(QueryType.class);
@@ -658,10 +654,6 @@ public abstract class TreeSitterAnalyzer implements IAnalyzer, TypeAliasProvider
 
     protected final ProgressListener getProgressListener() {
         return this.progressListener;
-    }
-
-    protected final AnalyzerCache getCache() {
-        return this.cache;
     }
 
     /**
@@ -3623,10 +3615,6 @@ public abstract class TreeSitterAnalyzer implements IAnalyzer, TypeAliasProvider
     protected abstract IAnalyzer newSnapshot(
             AnalyzerState state, ProgressListener listener, @Nullable AnalyzerCache previousCache);
 
-    protected AnalyzerCache createFilteredCache(AnalyzerCache previous, Set<ProjectFile> changedFiles) {
-        return new AnalyzerCache(previous, changedFiles);
-    }
-
     @Override
     public IAnalyzer update(Set<ProjectFile> changedFiles) {
         if (changedFiles.isEmpty()) return this;
@@ -3759,7 +3747,7 @@ public abstract class TreeSitterAnalyzer implements IAnalyzer, TypeAliasProvider
                 nextSymbolKeyIndex,
                 snapshotNanos);
 
-        var filteredCache = createFilteredCache(this.cache, changedFiles);
+        var filteredCache = new AnalyzerCache(this.cache, changedFiles);
         this.isStale = true;
         return newSnapshot(typedState, getProgressListener(), filteredCache);
     }
